acd7f3411a6af5a34fbb90f39c7f86eaecfe3ce6
Tomas <Tomas@IngenieriaCreativaDS3>
2018-05-23 16:27:15 -0300

    tercer commit, borrame

--- src/fotocopiadorav3/Vista/FXMLDocumentController.java
+++ src/fotocopiadorav3/Vista/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fotocopiadorav3;
+package fotocopiadorav3.Vista;
 
 import java.net.URL;
 import java.util.ResourceBundle;
