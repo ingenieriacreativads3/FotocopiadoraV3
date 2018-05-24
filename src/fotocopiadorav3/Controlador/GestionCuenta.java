0fe3e4176b584974ee4082d9fe00925b2c40d088
Usuario <Usuario@Fonsho>
2018-05-24 13:06:55 -0300

    Segundo commit

---
+++ src/fotocopiadorav3/Controlador/GestionCuenta.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fotocopiadorav3.Controlador;
+
+/**
+ * Esta clase maneja aspectos relacionados con el logueo e inicio de secion.
+ * 
+ * @author Alonso David
+ */
+public class GestionCuenta {
+    
+   protected void iniciarSecion(){
+       /**
+        * El metodo recibe por parametros un usuarioy una contraseña.
+        * En caso de ser datos validos, devuelve true.
+        * En case de ser datos incorrectos, devuelve false.
+        */
+   }
+   
+   
+   
+}
---
+++ src/fotocopiadorav3/Controlador/GestionPedido.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fotocopiadorav3.Controlador;
+
+/**
+ * Esta clase se encarga de las cuestiones relacionadas con la gestion de los
+ * pedidos. Crear pedidos, modificar pedidos, cancelar pedidos.
+ * 
+ * @author Alonso David
+ */
+public class GestionPedido {
+    
+    protected void CrearPedido(){
+     /**
+     * Metodo para crear un pedido. Recive por parametro un libro.
+     */
+    }
+    
+    
+    
+    
+}
