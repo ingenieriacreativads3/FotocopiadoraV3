3082d15766facda75752ec97cbc3c8dbdfd8a2dc
claudio <ingenieriacreativads3@gmail.com>
2018-10-31 10:39:44 -0300

    Merge origin/master
    Conflicts:
    	src/fotocopiadorav3/Vista2/Login.form
    	src/fotocopiadorav3/Vista2/Login.java
    	src/fotocopiadorav3/Vista2/Vista2Interfaz.java

---
+++ src/fotocopiadorav3/persistir una clase 2.java
@@ -0,0 +1,72 @@
+/*
+///HACER ESTO EN TODAS LAS CALSES
+
+///modificar la funcion
+
+private static int getLastId();
+
+///en la linea
+
+Estado estadoConsulta = getInformacion();
+
+///por la linea
+
+Estado estadoConsulta = Estado.EXITO;
+
+///ir a la clase 
+
+ConexionMySql.java
+
+///a la funcion
+
+getAllInformacion();
+
+///Agregar getInformacion de la clase modificada
+///terminar de modificar la funcion 
+
+getLastId();
+
+///poner todo dentro de in if
+
+if(listaObjetos != null);
+
+///Modificar la funcion 
+
+getForId();
+
+///cambiar la linea
+
+Estado seObtuvo = getInformacion();
+
+// por la linea
+
+Estado seObtuvo = Estado.EXITO;
+
+///poner todo en un if
+
+if(listaObjetos != null);
+
+///en la funcion 
+
+protected static AlfaNumerico nuevo(
+    String palabraRecibida);
+
+///comentar las lineas
+
+Estado seGuardo = objetoDevolver.guardar();
+
+///y el if que sigue
+
+if(seGuardo == Estado.EXITO);
+
+///la linea 
+
+listaObjetos.remove(objetoDevolver);
+
+///colocarla en el else siguiente del if
+
+if(seSeteo == Estado.EXITO);
+
+///ir a la funcion guardar y hacerla public
+
+*/
